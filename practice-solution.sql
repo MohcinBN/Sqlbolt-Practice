0c59fb3e05dcc624f2cf2a2d7c20cafc0bf8abd1
Mohcin Bounouara <rapmabiz@gmail.com>
2022-09-18 18:47:21 +0100

    update solution with "Queries with aggregates"
    Using Having and Where clause.

--- practice-solution.sql
+++ practice-solution.sql
@@ -15,3 +15,36 @@
  "SELECT building, SUM(years_employed) AS total
  FROM employees
  GROUP by building;"
+ 
+ 
+ --  SQL Lesson 11: Queries with aggregates (Pt. 2) 
+ 
+Find the number of Artists in the studio (without a HAVING clause):
+ 
+SELECT role, COUNT(role) as artists_numbers 
+FROM employees
+WHERE employees.role = "Artist";
+
+
+Find the number of Employees of each role in the studio 
+-- Without HAVING clause;
+SELECT role, COUNT(role) as artists_numbers 
+FROM employees
+WHERE employees.role = "Artist" OR employees.role = "Manager" OR employees.role = "Engineer"
+GROUP by role;
+
+-- With using having cleuse;
+SELECT role, COUNT(role) as artists_numbers 
+FROM employees
+WHERE employees.role = "Artist" OR employees.role = "Manager" OR employees.role = "Engineer"
+GROUP by role
+HAVING COUNT(role) > 0;
+
+
+
+-- Find the total number of years employed by all Engineers 
+SELECT role, SUM(years_employed) as total_years 
+FROM employees
+GROUP by role
+HAVING role = "Engineer";
+
